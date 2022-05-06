e2e06a361530bb8deffbcdb9a29d7e5d1863e466
migbernardo <mbdexb@gmail.com>
2022-05-06 23:18:01 +0100

    class + cycle crossover scripts

---
+++ class.py
@@ -0,0 +1,73 @@
+import numpy as np
+import random
+
+
+class Solutions:
+    def __init__(self, item, num_solutions):
+        self.item = item
+        self.rep = np.array(item)
+        # break puzzle into each respective sub-grid from left to right and top to bottom
+        # create a matrix representation of the flat sub-grids
+        self.subgrids = np.stack([
+            self.rep[:3, :3].flatten(),
+            self.rep[:3, 3:6].flatten(),
+            self.rep[:3, 6:].flatten(),
+            self.rep[3:6, :3].flatten(),
+            self.rep[3:6, 3:6].flatten(),
+            self.rep[3:6, 6:].flatten(),
+            self.rep[6:, :3].flatten(),
+            self.rep[6:, 3:6].flatten(),
+            self.rep[6:, 6:].flatten()
+        ], axis=0)
+
+        # get fixed and non-fixed indexes of each sub-grid
+        self.fixed_index = []
+        self.not_fixed_index = []
+        for row in range(self.subgrids.shape[0]):
+            self.fixed_index.append(list(np.where(self.subgrids[row] != 0)[0]))
+            self.not_fixed_index.append(list(np.where(self.subgrids[row] == 0)[0]))
+
+        # get fixed numbers of each sub-grid
+        self.fixed_num = []
+        for row, indexes in enumerate(self.fixed_index):
+            num = []
+            for index in indexes:
+                num.append(self.subgrids[row][index])
+            self.fixed_num.append(num)
+
+        # get list of numbers to select for each sub-grid
+        self.num_to_sel = []
+        for numbers in self.fixed_num:
+            pop = list(range(1, 10))
+            for number in numbers:
+                pop.remove(number)
+            self.num_to_sel.append(pop)
+
+        # generate a set of solutions
+        self.solutions = []
+        for n_sol in range(num_solutions):
+            sol = self.subgrids.copy()
+            # sample list of numbers to select for each sub-grid
+            self.sel_num = []
+            for numbers in self.num_to_sel:
+                self.sel_num.append(random.sample(numbers, k=len(numbers)))
+
+            # replaced non-fixed positions with selected numbers (generate a solution)
+            for row in range(sol.shape[0]):
+                for i, index in enumerate(self.not_fixed_index[row]):
+                    sol[row][index] = self.sel_num[row][i]
+            self.solutions.append(sol)
+
+
+if __name__ == '__main__':
+    puzzle = Solutions(item=[
+        [8, 0, 2, 0, 0, 3, 5, 1, 0],
+        [0, 6, 0, 0, 9, 1, 0, 0, 3],
+        [7, 0, 1, 0, 0, 0, 8, 9, 4],
+        [6, 0, 8, 0, 0, 4, 0, 2, 1],
+        [0, 0, 0, 2, 5, 8, 0, 6, 0],
+        [9, 2, 0, 3, 1, 0, 4, 0, 0],
+        [0, 0, 0, 4, 0, 2, 7, 8, 0],
+        [0, 0, 5, 0, 8, 9, 0, 0, 0],
+        [2, 0, 0, 0, 0, 7, 1, 0, 0]
+    ], num_solutions=5)
---
+++ cycle_co.py
@@ -0,0 +1,42 @@
+import random
+
+
+def shuffle(parent, not_fixed_index):
+    num_to_shuffle = [parent[index] for index in not_fixed_index]
+    shuffled = random.sample(num_to_shuffle, k=len(num_to_shuffle))
+
+    for i, j in enumerate(not_fixed_index):
+        parent[j] = shuffled[i]
+
+    return parent
+
+
+def cycle_co(p1, p2):
+    c1 = [None] * len(p1)
+    c2 = [None] * len(p2)
+
+    while None in c1:
+        index = c1.index(None)
+        if index != 0:
+            p1, p2 = p2, p1
+        val1 = p1[index]
+        val2 = p2[index]
+
+        while val1 != val2:
+            c1[index] = p1[index]
+            c2[index] = p2[index]
+            val2 = p2[index]
+            index = p1.index(val2)
+
+        c1[index] = p1[index]
+        c2[index] = p2[index]
+    return c1, c2
+
+
+if __name__ == '__main__':
+    p1 = [8, 4, 2, 5, 6, 9, 7, 3, 1]
+    p2 = [8, 4, 2, 9, 6, 5, 7, 3, 1]
+    p1 = shuffle(p1, [1, 3, 5, 7])
+    p2 = shuffle(p2, [1, 3, 5, 7])
+    c1 = cycle_co(p1, p2)[0]
+    c2 = cycle_co(p1, p2)[1]
